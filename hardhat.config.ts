8b828d99671a4d5288931cb8338ff2e6d1bc68af
Nicolas Ortiz <niortiz51@gmail.com>
2023-07-24 23:52:27 +1000

    commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+//const INFURA_API_KEY: string | undefined = process.env.INFURA_API_KEY;
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+//const PRIVATE_KEY: string| undefined = process.env.WALLET_PRIVATE_KEY;
+const { PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.18",
   networks: {
-    ganache: {
-        url: "http://127.0.0.1:8545",
-        accounts:
-             ['0xfe3a94ace3e6e1b85e0339b310f534051ecccd8988245c5112932c83a382347e'],
-        },
+    hardhat: {
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      allowUnlimitedContractSize: true
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com`, 
+      accounts: [PRIVATE_KEY]
+    }
+  },
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
     },
 };
 
